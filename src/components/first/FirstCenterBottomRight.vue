a18e153fb3afef197f443049ed2261e03ac6bb74
xu.xinying <xu.xinying@ani.cloud>
2019-02-24 09:57:04 +0800

    front

--- src/components/first/FirstCenterBottomRight.vue
+++ src/components/first/FirstCenterBottomRight.vue
@@ -36,70 +36,7 @@
 
   </div>
 </template>
-<script>
-    export default {
-        data () {
-            return {
-                option: {
-                    tooltip: {
-                        trigger: 'item',
-                        formatter: "{a} <br/>{b}: {c} ({d}%)"
-                    },
-                    grid: {
-//                        show: false
-                        x: 1,
-                        y: 1,
-                        x2: 1,
-                        y2: 1,
-                        // containLabel: true
-                    },
-                    series: [
-                        {
-                            type:'pie',
-                            radius: ['62%', '70%'],
-                            avoidLabelOverlap: false,
-                            label: {
-                                normal: {
-                                    show: false,
-                                    position: 'center'
-                                },
-                                emphasis: {
-                                    show: true,
-                                    textStyle: {
-                                        fontSize: '10',
-                                        fontWeight: 'bold'
-                                    }
-                                }
-                            },
-                            labelLine: {
-                                normal: {
-                                    show: false
-                                }
-                            },
-                            data:[
-                                {value:335, name:'直接访问', color:'#bf025c'},
-                                {value:102, name:'邮件营销', color:'#14182e'},
-                            ]
-                        }
-                    ]
-                }
-            }
-        },
-        props: [
-            'title',
-        ],
-        mounted() {
-        },
-        computed: {
-            getColorClass: function () {
-                return {
-                    'title-color': this.isBlue,
-                }
-            }
-        }
-    }
-</script>
-
+<script src="./FirstCenterBottomRight.js"></script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
   .content{
--- src/components/second/SecondTopLeft.vue
+++ src/components/second/SecondTopLeft.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="content">
-    {{msg}}
+    <div class="content-left">
+
+    </div>
+    <div class="content-right">
+
+    </div>
   </div>
 </template>
 <script>
@@ -21,5 +26,20 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-
+  .content{
+    width: 100%;
+    height: 88%;
+    border:1px solid grey;
+    display: flex;
+    .content-left{
+      width: 85%;
+      height: 100%;
+      border:1px solid grey;
+    }
+    .content-right{
+      width: 15%;
+      height: 100%;
+      border:1px solid grey;
+    }
+  }
 </style>
